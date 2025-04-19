2878a9567e64b5e1014d58b56fb414ed9ef7fb7d
thewisepup <wisepup257@gmail.com>
2025-04-19 13:53:49 -0700

    integrate supabase to get podcast info and update podcast status

--- podcast_scripty.py
+++ podcast_scripty.py
@@ -1,27 +1,53 @@
 #!/usr/bin/env python3
 
 import argparse
+import os
+from dotenv import load_dotenv
+from supabase import Client, create_client
+
+load_dotenv()
+supabase_url = os.environ.get("SUPABASE_URL")
+supabase_key = os.environ.get("SUPABASE_KEY")
 
 
-def main():
+supabase: Client = create_client(supabase_url, supabase_key)
+
+
+def parse_args():
     # Create argument parser
     parser = argparse.ArgumentParser(description="Process user ID and prompt")
 
-    # Add required arguments
-    parser.add_argument("--userId", required=True, help="User ID")
-    parser.add_argument("--prompt", required=True, help="User prompt")
+    parser.add_argument("--podcastId", required=True, help="Podcast ID")
 
     # Parse arguments
     args = parser.parse_args()
 
-    # Access the arguments
-    user_id = args.userId
-    user_prompt = args.prompt
+    podcast_id = args.podcastId
 
-    # Print the received values (you can replace this with your actual logic)
-    print(f"User ID: {user_id}")
-    print(f"User Prompt: {user_prompt}")
+    return podcast_id
+
+
+def generate_podcast(podcast_id):
+    response = supabase.table("podcasts").select("*").eq("id", podcast_id).execute()
+    podcast = response.data[0]
+    user_id = podcast["user_id"]
+    user_description = podcast["user_description"]
+    print(f"Generating podcast for user {user_id} with prompt {user_description}")
+    # TODO: generate podcast
+    # TODO: upload podcast to s3/supabase
+
+    response = (
+        supabase.table("podcasts")
+        .update({"status": "READY"})
+        .eq("id", podcast_id)
+        .execute()
+    )  # update_location with s3 location
 
 
 if __name__ == "__main__":
-    main()
+    podcast_id = parse_args()
+    # Print the received values (you can replace this with your actual logic)
+
+    print(f"Podcast ID: {podcast_id}")
+
+    generate_podcast(podcast_id)
